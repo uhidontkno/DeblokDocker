a165e8ff00c5a8a6239784abdca884b85de25ee7
uhidontkno <hi@rare1k.dev>
2024-02-16 15:12:13 -0500

    fix alpine (again)

--- alpine-base/start.sh
+++ alpine-base/start.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # Start the VNC server
 Xvfb :1 -screen 0 640x480x24 &
-bash ./xstartup
+
 sleep 1
 x11vnc -display :1 -forever -usepw &
+export DISPLAY=:1
+bash ./xstartup
 /app/noVNC/utils/novnc_proxy --vnc localhost:5900
