746129313753656a6611e9ca69c265c4aa27597b
uhidontkno <hi@rare1k.dev>
2024-02-16 15:03:40 -0500

    start xterm & fix arch

--- alpine-base/start.sh
+++ alpine-base/start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Start the VNC server
 Xvfb :1 -screen 0 640x480x24 &
+bash ./xstartup
 sleep 1
 x11vnc -display :1 -forever -usepw &
 /app/noVNC/utils/novnc_proxy --vnc localhost:5900
