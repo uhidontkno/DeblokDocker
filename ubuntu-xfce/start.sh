0565be0a3b1da56c3283599aa1516b75179a3dae
uhidontkno <hi@rare1k.dev>
2024-02-17 13:20:33 -0500

    probably need sudo

--- ubuntu-xfce/start.sh
+++ ubuntu-xfce/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 vncserver -kill :1
-rm -rf /run/dbus
-mkdir -p /run/dbus
+sudo rm -rf /run/dbus
+sudo mkdir -p /run/dbus
 resolution="${2:-960x720}"
 vncserver :1 -geometry "$resolution" -depth 24 &
 /app/noVNC/utils/novnc_proxy --vnc localhost:5901
