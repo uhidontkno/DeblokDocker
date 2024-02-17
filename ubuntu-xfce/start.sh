bfc5f8876f3cf6ecb3c22829a991d7926c07ca60
uhidontkno <hi@rare1k.dev>
2024-02-17 14:48:01 -0500

    xvfb fixes everything

--- ubuntu-xfce/start.sh
+++ ubuntu-xfce/start.sh
@@ -2,6 +2,7 @@
 vncserver -kill :1
 sudo rm -rf /run/dbus
 sudo mkdir -p /run/dbus
+sleep 1
 resolution="${2:-960x720}"
 vncserver :1 -geometry "$resolution" -depth 24 &
 /app/noVNC/utils/novnc_proxy --vnc localhost:5901
