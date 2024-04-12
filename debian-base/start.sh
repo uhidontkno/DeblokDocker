3c0bf6f3f6e68818c3740f09850a531ef4a5eed5
rare1k <hi@rare1k.dev>
2024-04-11 20:58:58 -0400

    fix: remove old code

--- debian-base/start.sh
+++ debian-base/start.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 vncserver :1 -geometry 1280x720 -depth 24 &
-sudo rm -rf /run/dbus
-sudo mkdir -p /run/dbus
 xsetroot -bg black -fg white
 /app/noVNC/utils/novnc_proxy --vnc localhost:5901
