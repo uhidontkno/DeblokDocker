fe1e5163e55913267e5ff86d1fcb5e851c893e94
rare1k <hi@rare1k.dev>
2024-04-11 20:40:40 -0400

    fix: some fixes

--- alpine-base/start.sh
+++ alpine-base/start.sh
@@ -3,7 +3,7 @@
 Xvfb :1 -screen 0 1280x720x24 &
 
 sleep 1
-x11vnc -display :1 -forever -usepw -br &
+x11vnc -display :1 -forever -usepw &
 export DISPLAY=:1
 bash ./xstartup
 /app/noVNC/utils/novnc_proxy --vnc localhost:5900
--- ubuntu-base/start.sh
+++ ubuntu-base/start.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
+vncserver :1 -geometry 1280x720 -depth 24 &
 xsetroot -bg black -fg white
-
-# Start the VNC server
-vncserver :1 -geometry 1280x720 -depth 24 -br &
 /app/noVNC/utils/novnc_proxy --vnc localhost:5901
