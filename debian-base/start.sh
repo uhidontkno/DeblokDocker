5bd5d1ce045879a1d51e34d3466fea3e8e93bfb3
rare1k <hi@rare1k.dev>
2024-04-18 20:24:57 -0400

    feat: get tigervnc working with remote resize

--- debian-base/start.sh
+++ debian-base/start.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-Xvfb :1 +extension RANDR -screen 1280x720x16 &
-export DISPLAY=:1
-bash ./xstartup &
+vncserver :1 -passwd /root/.vnc/passwd &
 sleep 1
-x11vnc -display :1 -nopw &
-/app/noVNC/utils/novnc_proxy --vnc localhost:5900
+/app/noVNC/utils/novnc_proxy --vnc localhost:5901
