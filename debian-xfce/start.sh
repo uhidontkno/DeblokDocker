814e0e276bc79608dfd3ce074bf6363997be68cb
rare1k <hi@rare1k.dev>
2024-04-18 20:33:09 -0400

    feat: tigervnc with remote resizing for everything else

--- debian-xfce/start.sh
+++ debian-xfce/start.sh
@@ -3,6 +3,6 @@ vncserver -kill :1
 sudo rm -rf /run/dbus
 sudo mkdir -p /run/dbus
 sleep 1
-resolution="${1:-1280x720}"
-vncserver :1 -geometry "$resolution" -depth 24 &
+vncserver :1 -passwd /root/.vnc/passwd &
+sleep 1
 /app/noVNC/utils/novnc_proxy --vnc localhost:5901
