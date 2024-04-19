4c68bbd005104bad094baa449d48f0d5b3445e95
rare1k <hi@rare1k.dev>
2024-04-18 23:17:41 -0400

    fix: attempt to fix chromium and firefox

--- firefox/start.sh
+++ firefox/start.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 vncserver :1 -passwd /root/.vnc/passwd &
 sleep 1
-/app/noVNC/utils/novnc_proxy --vnc localhost:5901
+/app/noVNC/utils/novnc_proxy --vnc localhost:5901 &
+sleep 3
+echo "openbox-session &" | bash
+#sleep 1
+#nitrogen ./blank.png
+sleep 1
+echo "while :
+do
+firefox --no-sandbox
+done
+" | bash 
