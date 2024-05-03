506ea1f6d2a20bc17a8bfd84d10b53aa4f910768
rare1k <hi@rare1k.dev>
2024-05-02 22:05:29 -0400

    feat: overhaul pull script

--- easydeploy/deblokmanager/pull.sh
+++ easydeploy/deblokmanager/pull.sh
@@ -1,9 +1,43 @@
 
-docker pull ghcr.io/deblok-workshop/deblokdocker/alpine-base:latest > /dev/null
-docker pull ghcr.io/deblok-workshop/deblokdocker/debian-base:latest > /dev/null
-docker pull ghcr.io/deblok-workshop/deblokdocker/fedora-base:latest > /dev/null
-docker pull ghcr.io/deblok-workshop/deblokdocker/ubuntu-base:latest > /dev/null
-docker pull ghcr.io/deblok-workshop/deblokdocker/debian-xfce:latest > /dev/null
-docker pull ghcr.io/deblok-workshop/deblokdocker/chromium:latest  > /dev/null
-docker pull ghcr.io/deblok-workshop/deblokdocker/firefox:latest > /dev/null
-docker pull ghcr.io/deblok-workshop/deblokdocker/ubuntu-xfce:latest > /dev/null
+# Script colors
+OFF='\033[0m'       # Text Reset
+
+# Regular Colors
+BLACK='\033[0;30m'        # BLACK
+RED='\033[0;31m'          # RED
+GREEN='\033[0;32m'        # GREEN
+YELLOW='\033[0;33m'       # YELLOW
+BLUE='\033[0;34m'         # BLUE
+PURPLE='\033[0;35m'       # PURPLE
+CYAN='\033[0;36m'         # CYAN
+WHITE='\033[0;37m'        # WHITE
+
+# Bold
+BBLACK='\033[1;30m'       # BLACK
+BRED='\033[1;31m'         # RED
+BGREEN='\033[1;32m'       # GREEN
+BYELLOW='\033[1;33m'      # YELLOW
+BBLUE='\033[1;34m'        # BLUE
+BPURPLE='\033[1;35m'      # PURPLE
+BCYAN='\033[1;36m'        # CYAN
+BWHITE='\033[1;37m'       # WHITE
+
+printf "$OFF$CYAN pull:$OFF$BCYAN pulling alpine-base...$OFF\n"
+docker pull ghcr.io/deblok-workshop/deblokdocker/alpine-base:latest > /dev/null 2> /dev/null
+
+printf "$OFF$CYAN pull:$OFF$BCYAN pulling debian-base...$OFF\n"
+docker pull ghcr.io/deblok-workshop/deblokdocker/debian-base:latest > /dev/null 2> /dev/null
+
+printf "$OFF$CYAN pull:$OFF$BCYAN pulling fedora-base...$OFF\n"
+docker pull ghcr.io/deblok-workshop/deblokdocker/fedora-base:latest > /dev/null 2> /dev/null
+
+printf "$OFF$CYAN pull:$OFF$BCYAN pulling ubuntu-base...$OFF\n"
+docker pull ghcr.io/deblok-workshop/deblokdocker/ubuntu-base:latest > /dev/null 2> /dev/null 
+
+printf "$OFF$CYAN pull:$OFF$BCYAN pulling xfce containers...$OFF\n"
+docker pull ghcr.io/deblok-workshop/deblokdocker/debian-xfce:latest > /dev/null 2> /dev/null
+docker pull ghcr.io/deblok-workshop/deblokdocker/ubuntu-xfce:latest > /dev/null 2> /dev/null
+
+printf "$OFF$CYAN pull:$OFF$BCYAN pulling single app containers...$OFF\n"
+docker pull ghcr.io/deblok-workshop/deblokdocker/chromium:latest  > /dev/null 2> /dev/null
+docker pull ghcr.io/deblok-workshop/deblokdocker/firefox:latest > /dev/null 2> /dev/null
